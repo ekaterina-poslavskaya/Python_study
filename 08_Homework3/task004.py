2d17c543426d276520c89961804f414f5db45ca5
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-10-03 09:49:35 +0300

    divmod вместо // и %

--- 08_Homework3/task004.py
+++ 08_Homework3/task004.py
@@ -11,8 +11,10 @@ num = int(input('Введит десятичное число..'))
 num_abs = abs(num)
 s = ''
 while num_abs != 0 and num_abs != 1:
-    s = str(num_abs % 2) + s
-    num_abs = num_abs // 2
+    #s = str(num_abs % 2) + s
+    #num_abs = num_abs // 2
+    num_abs,b = divmod(num_abs, 2) #вместо предыдущих двух строк можно еще и так
+    s = str(b) + s
 s = str(num_abs) + s
 if num < 0:
     s = '-' + s
